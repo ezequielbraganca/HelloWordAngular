31cccc07a9bad37ea859dc70c24236842b761413
Ezequiel Braganca <ezequielbraganca@hotmail.com>
2019-04-11 08:35:35 -0300

    Curso Angular2-Aula3-components

---
+++ src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+import { template } from '@angular/core/src/render3';
+
+
+@Component({
+    selector:'meu-primeiro-component',
+    template: '<p>Meu Primeiro component com Angular 2!</p>' 
+})
+export class MeuPrimeiroComponent {}
